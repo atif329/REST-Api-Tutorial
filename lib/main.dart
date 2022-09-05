f59f14a635bb5deb7bf8fb68b891f5feca77bc47
atif329 <atif05017@gmail.com>
2022-09-06 00:25:17 +0500

    fifth example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:api_learning/example_four.dart';
-import 'package:api_learning/expample_five.dart';
-import 'package:api_learning/third_example.dart';
+// import 'package:api_learning/example_four.dart';
+import 'package:api_learning/fifth_example.dart';
+// import 'package:api_learning/third_example.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const FifthExample(),
+      home: const LastExampleScreen(),
     );
   }
 }
